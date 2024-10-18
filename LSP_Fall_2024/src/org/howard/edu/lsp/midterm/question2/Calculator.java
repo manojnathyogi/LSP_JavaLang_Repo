9ebcd8708a6b2290c54c7af2e45431a3993a06ee
manojnathyogi <manojnathyogi@10.0.0.165>
2024-10-17 20:08:21 -0400

    corrected calculator.java file

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
@@ -17,13 +17,6 @@ public class Calculator {
         for (int num : arr) {
             total += num;
         }
-        return total;public class CalculatorDriver {
-            public static void main(String[] args) {
-                System.out.println(Calculator.sum(5, 10));            // Output: 15
-                System.out.println(Calculator.sum(3.5, 7.8));         // Output: 11.3
-                System.out.println(Calculator.sum(new int[]{1, 2, 3, 4, 5})); // Output: 15
-            }
-        }
-
+        return total;
     }
 }
